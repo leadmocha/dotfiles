0cd37d5e60ee17b46a31d5aa7e5e600817f1c44f
Juan Carlos Cornejo <cornejo@jlab.org>
2016-05-12 08:39:38 -0400

    Some useful bash utilities that I find I need to do often. Finally just wrote them into functions.
    1) Source a file only if it exits.
    2) Cleanly add a path to an environmental variable (by removing previous instances).

---
+++ shell/jc2_utils.sh
@@ -0,0 +1,62 @@
+###############################################################################
+## JC2 useful utilities
+
+## Source file only if it exists
+function sourceIfExists()
+{
+  if [ -f $1 ];
+  then
+    source $1
+  fi
+}
+
+###############################################################################
+## Cleanly modify paths stored in a variable and separated by colons
+pathRemove()
+{
+  ## Dereference the variable
+  var=\$"$1"
+  ## Get variable contents
+  val=`eval "expr \"${var}\" "`
+  ## Now remove the given path from this variable
+  eval "$1=`echo -n ${val} | awk -v RS=: -v ORS=: '$0 != "'$2'"' | sed 's/:$//'`"
+}
+pathAddClean()
+{
+  ## Dereference the variable
+  var=\$"$1"  ## Deferencing the variable
+  ## Get variable contents
+  val=`eval "expr \"${var}\" "`
+  pathRemove $1 $2
+  ## Get the variable contents again
+  val=`eval "expr \"${var}\" "`
+
+  ## Check to see if it's empty or just a single colon
+  if [ -z ${val} ]; # || [ ${val} = "x" ];
+  then
+    ## Variable is empty, so we can just add the path
+    eval "$1=\"$2\""
+  else
+    ## Variable already has content, so add this path
+    #if [ $3 == "prepend" ];
+    if [ -z $3 ] || [ $3 = "prepend" ];
+    then
+      ## Prepend the path
+      eval "$1=\"$2:${val}\""
+    else
+      ## Append the path
+      eval "$1=\"${val}:$2\""
+    fi
+  fi
+}
+
+pathPrependClean()
+{
+  pathAddClean $1 $2 prepend
+}
+
+pathAppendClean()
+{
+  pathAddClean $1 $2 append
+}
+
